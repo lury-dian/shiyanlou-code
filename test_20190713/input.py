988a7e09c8bffa03a028976e891d1beb8fb9dbb0
lury-dian <53420856+lury-dian@users.noreply.github.com>
2019-07-31 17:22:05 +0800

    Update input.py
    这个理由有三个脚本，添加备注标明每个脚本实现的内容

--- test_20190713/input.py
+++ test_20190713/input.py
@@ -1,3 +1,5 @@
+
+#计算平均值
 #! /usr/bin/env python3
 n = int(input("how many numbers do you want"))
 sum = 0
@@ -11,11 +13,15 @@ while count < n:
                 print("N={},Sum ={}".format(n,sum))
                 print("Average=={:.2f}".format(average))
 
+
+#根据总天数计算月数和天数
 #! /usr/bin/env python3
 days = int(input("Enter days: "))
 print("Months = {} Days = {}".format(*divmod(days,30)))
 #divmod(num1,num2) can return tuple,this tuple contains two values, one is num1//num2, other one is num1%num2, then use * to unpacking this tuple.
 
+
+#将摄氏度转换成华氏度
 #! /usr/bin/env python3
 fahrenheit=0
 print("Fahrenheit Celsius")
