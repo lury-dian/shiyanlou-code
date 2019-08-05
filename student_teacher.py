f0eb354468c9dd68081a3c017f396d020a59e760
lury-dian <dianyuh@sina.com>
2019-08-05 14:13:04 +0800

    继承类，使用Join将元组转成字符串，使用Counter来处理字符串，每个字符分别有几个得到列表，使用i,j in 列表来处理里面中的元素是类似于('a',1)这样的元组，对元组内的数据进行处理”

---
+++ student_teacher.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python3
+import sys
+from collections import Counter
+
+class Person(object):
+    """
+    返回具有给定名称的 Person 对象
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_details(self):
+        """
+        返回包含人名的字符串
+        """
+        return self.name
+
+    def get_grade(self):
+        return 0
+
+
+class Student(Person):
+    """
+    返回 Student 对象，采用 name, branch, year 3 个参数
+    """
+
+    def __init__(self, name, branch, year,grade):
+        Person.__init__(self, name)
+        self.branch = branch
+        self.year = year
+        self.grade = grade
+
+    def get_details(self):
+        """
+        返回包含学生具体信息的字符串
+        """
+        return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
+
+    def get_grade(self):
+        comm = Counter(self.grade).most_common(4)
+        n1=0
+        n2=0
+        for i in comm:
+            if i[0] != 'D':
+                n1 += i[1]
+            else:
+                n2 += i[1]
+        print("Pass: {}, Fail: {}".format(n1,n2))
+
+class Teacher(Person):
+    """
+    返回 Teacher 对象，采用字符串列表作为参数
+    """
+    def __init__(self, name, papers,grade):
+        Person.__init__(self, name)
+        self.papers = papers
+        self.grade = grade
+
+    def get_details(self):
+        return "{} teaches {}".format(self.name, ','.join(self.papers))
+
+    def get_grade(self):
+        list1 =[]
+        comm = Counter(self.grade).most_common(4)
+        for i,j in comm:
+            list1.append("{}:{}".format(i,j))
+        print(','.join(list1))
+
+
+person1 = Person('Sachin')
+if sys.argv[1] == 'student':
+    student1 = Student('Kushal', 'CSE', 2005,sys.argv[2])
+    student1.get_grade()
+else:
+    teacher1 = Teacher('Prashad', ['C', 'C++'],sys.argv[2])
+    teacher1.get_grade()
