508ce4befaba90014244ca23ca2d284cd006fee9
lury-dian <dianyuh@sina.com>
2019-08-06 16:55:18 +0800

    this is a project for factorial

---
+++ factorial/myfact/__init__.py
@@ -0,0 +1,3 @@
+from fact import factorial
+__all__ = [factorial, ]
+#表示在fact模块中只有factorial函数能够导入到其他的模块中使用
---
+++ factorial/setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+"""Factorial project"""
+from setuptools import find_packages, setup
+
+setup(name='lury-up',
+        version='0.2',
+        description="Factorial module.",
+        long_description = "A test module for our book.",
+        platforms = ["Linux"],
+        author = "Lury",
+        author_email="dianyuh@sina.com",
+        url="https://github.com/lury-dian/shiyanlou-code",
+        license = "MIT",
+        packages= find_packages()
+        )
